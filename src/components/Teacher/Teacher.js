8841ea2e75b53419b4c00abd61db2baa2e053f9e
ankan-782 <royavijit782@gmail.com>
2021-10-04 23:42:28 +0600

    teacher page edited

--- src/components/Teacher/Teacher.js
+++ src/components/Teacher/Teacher.js
@@ -11,6 +11,11 @@ const Teacher = (props) => {
                 </div>
                 <div className="card-body mb-5 text-center">
                     <h1 className="card-title">{name}</h1>
+                    <div className="d-flex justify-content-center align-items-center mt-4">
+                        <button className="btn ms-2"><i className="fab fa-facebook-f fs-5"></i></button>
+                        <button className="btn ms-2"><i className="fab fa-linkedin-in fs-5"></i></button>
+                        <button className="btn ms-2"><i className="fab fa-twitter fs-5"></i></button>
+                    </div>
                 </div>
                 <div className="text-center px-5">
                     <p>Takes classes of:</p>
